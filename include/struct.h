9e68c1d89e7d798daf53426c4c69a5ab734e3f03
Giacomo <jacky.trave@icloud.com>
2024-05-07 11:19:03 +0200

    Aggiornata struct

--- include/struct.h
+++ include/struct.h
@@ -1,12 +1,5 @@
-struct Punto
-{
-    short unsigned int x;
-    short unsigned int y;
-    short unsigned int tipo; // 0: acqua, 1: nave.
-};
-
 struct Campo
 {
     short unsigned int dimensione;
-    Punto** campo;
+    short unsigned int** valore; // 0: acqua, 1: nave.
 };
